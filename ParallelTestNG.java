033e80457bd0e45099666105efd6cdbfe96c32bd
Sheba-John <92512577+Sheba-John@users.noreply.github.com>
2021-11-22 14:40:42 +0530

    Add files via upload

---
+++ ParallelTestNG.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+public class ParallelTestNG {
+	WebDriver driver;
+	
+	
+	@Test()
+	
+public void test1() throws InterruptedException {
+	System.out.println("I am inside test 1"+Thread.currentThread().getId());
+	//String projectPath = System.getProperty("user.dir");
+	System.setProperty("webdriver.gecko.driver","C:\\driver\\geckodriver.exe");
+	driver = new FirefoxDriver();
+	driver.get("https://opensource-demo.orangehrmlive.com/");
+	Thread.sleep(3000);
+	driver.close();
+	
+}
+	@Test()
+public void test2() throws InterruptedException {
+	System.out.println("I am inside test 2"+Thread.currentThread().getId());
+	//String projectPath = System.getProperty("user.dir");
+	System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(3000);
+		driver.close();
+	}	
+		@AfterClass
+		public void close()
+		{
+			driver.quit();
+		}
+		
+
+}
